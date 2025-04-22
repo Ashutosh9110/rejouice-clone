f4dbfee52b678697eca2c51b50db32c9aecf5d09
Ashutosh Adhikari <veni_vidi.vici@outlook.com>
2025-04-22 16:57:32 +0530

    lfjslfj

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-var page1Content = document.querySelector("#page1-content")
+function cursorEffect() {
+
+
+  var page1Content = document.querySelector("#page1-content")
 var cursor = document.querySelector("#cursor")
 
 
@@ -16,16 +19,18 @@ page1Content.addEventListener("mousemove", function(dets) {
 
 page1Content.addEventListener("mouseenter", function() {
   gsap.to(cursor, {
-    scale:1 
+    scale:1 ,
+    opacity:1
 })
 })
 
 page1Content.addEventListener("mouseleave", function() {
   gsap.to(cursor, {
-    scale:0
+    scale:0,
+    opacity:0
   })
 })
 
+}
 
-
-
+cursorEffect()
